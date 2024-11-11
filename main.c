df1307f778525ba84e4678270e4ba537efc2a9f7
FaMe1337 <167479661+FaMe1337@users.noreply.github.com>
2024-11-11 17:29:22 +0000

    and another one

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: famendes <famendes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 15:15:56 by famendes          #+#    #+#             */
-/*   Updated: 2024/11/11 15:06:14 by famendes         ###   ########.fr       */
+/*   Updated: 2024/11/11 17:22:31 by famendes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,10 @@ void	child_process(char **av, char **envp, int *fd)
 	close(fd[0]);
 	filein = open(av[1], O_RDONLY);
 	if (filein < 0)
+	{
+		close(fd[1]);
 		error("Failed opening filein");
+	}
 	dup2(fd[1], STDOUT_FILENO);
 	close(fd[1]);
 	dup2(filein, STDIN_FILENO);
@@ -34,7 +37,10 @@ void	parent_process(char **av, char **envp, int *fd)
 	close(fd[1]);
 	fileout = open(av[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
 	if (fileout < 0)
+	{
+		close(fd[0]);
 		error("Failed opening fileout");
+	}
 	dup2(fd[0], STDIN_FILENO);
 	close(fd[0]);
 	dup2(fileout, STDOUT_FILENO);
@@ -50,18 +56,19 @@ int	main(int ac, char **av, char **envp)
 	if (ac == 5)
 	{
 		if (av[2][0] == 0 || av[3][0] == 0)
-		{
-			ft_putstr_fd("Wrong arguments, please struc like the following example: ./pipex <file1> <cmd1> <cmd2> <file2>\n", 2);
-			exit(1);
-		}
+			error("cmd is empty");
+		if (!envp[0])
+			error("Envp path doesnt exist");
 		if (pipe(fd) < 0)
-			error("pipe creation failed\n");
+			error("pipe creation failed");
 		pid = fork();
 		if (pid == -1)
-			error("creation of forked failed\n");
+			error("creation of forked failed");
 		else if (pid == 0)
+		{
 			child_process(av, envp, fd);
-		waitpid(pid, NULL, 0);
+			waitpid(pid, NULL, 0);
+		}
 		parent_process(av, envp, fd);
 	}
 	else
